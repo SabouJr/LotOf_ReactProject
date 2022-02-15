10232a0d54cb5d108b96dbd30885984395d02a3c
clement.sabourin <clement.sabourin@eemi.com>
2022-02-15 15:23:59 +0100

    add context + click

--- Multi-langues/src/Components/Contenu/Contenu.js
+++ Multi-langues/src/Components/Contenu/Contenu.js
@@ -1,12 +1,16 @@
 import React, {useContext} from 'react'
 import './Contenu.css'
 import data from '../../assets/data'
+import { Context } from '../../context/LangContext'
 
 export default function Contenu() {
+
+  const {lang} = useContext(Context)
+
   return (
     <div className="content">
-      <h1 className="title">{data['FR'].title}</h1>
-      <p className="content-txt">{data['FR'].txt}</p>
+      <h1 className="title">{data[lang].title}</h1>
+      <p className="content-txt">{data[lang].txt}</p>
     </div>
   )
 }
--- Multi-langues/src/Components/ToggleLangs/ToggleLangs.js
+++ Multi-langues/src/Components/ToggleLangs/ToggleLangs.js
@@ -3,13 +3,15 @@ import FrenchFlag from '../../assets/france.svg'
 import SpanishFlag from '../../assets/spain.svg'
 import EnglishFlag from '../../assets/united-kingdom.svg'
 import './ToggleLangs.css'
+import { Context } from '../../context/LangContext'
 
 export default function ToggleLangs() {
+  const {ToggleLang} = useContext(Context)
   return (
     <div className='container-langs'>
-        <img src={FrenchFlag}/>
-        <img src={SpanishFlag}/>
-        <img src={EnglishFlag}/>
+        <img onClick={() => ToggleLang('FR')} src={FrenchFlag}/>
+        <img onClick={() => ToggleLang('ES')} src={SpanishFlag}/>
+        <img onClick={() => ToggleLang('EN')} src={EnglishFlag}/>
     </div>
   )
 }
