e2655ac08e5b3c1c9d9981c60041ac30bf169032
clement.sabourin <clement.sabourin@eemi.com>
2022-02-15 15:36:38 +0100

    Add browser Language

--- Multi-langues/src/context/LangContext.js
+++ Multi-langues/src/context/LangContext.js
@@ -2,8 +2,16 @@ import React, {createContext, useState} from 'react'
 
 export const Context = createContext();
 
+const supportedLang = ['EN', 'FR', 'ES'];
+
+let browserLang = navigator.language.slice(0,2).toUpperCase();
+
+if(supportedLang.indexOf(browserLang) === -1){
+  browserLang = 'EN';
+}
+
 const ContextProvider = props => {
-  const [lang, setLang] = useState('EN')
+  const [lang, setLang] = useState(browserLang)
   const ToggleLang = changeLang => {
     setLang(changeLang)
   }
