3303684db8851bdca323aa08bbb63811994ee62e
clement.sabourin <clement.sabourin@eemi.com>
2022-02-17 10:34:49 +0100

    add Indicator + Cards

---
+++ Multi-step-Form/src/Components/MultiForm/MultiForm.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+import './MultiForm.css'
+import Indicator from './Indicator/Indicator'
+import CardBegin from './Infos/CardBegin'
+import CardEnd from './Infos/CardEnd'
+
+export default function MultiForm() {
+
+  const [formIndex, setFormIndex] = useState(1)
+  const [allFormData, setAllFormData] = useState({
+    dietForm: "",
+    foodStyle: [],
+    allergies: [],
+    prefs: {}
+  })
+
+  return (
+    <div className="container-multiform">
+      <Indicator/>
+    </div>
+  )
+}
