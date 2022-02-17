2bc8da468f1583cd8601f3c5020c67a926111c2c
clement.sabourin <clement.sabourin@eemi.com>
2022-02-17 11:00:13 +0100

    add first card

---
+++ Multi-step-Form/src/Components/MultiForm/SubForms/DietForm.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+import './SubForm.css'
+
+export default function DietForm(props) {
+
+  const handleRadio = () => {
+
+  }
+  const preventFunc = e => e.preventDefault()
+
+  return (
+    <form onSubmit={preventFunc} className="diet-form">
+      <p>Quelle est ton régime alimentaire ?</p>
+
+      <label htmlFor="nodiet">Pas de régime particulier</label>
+      <input type="radio" onChange={handleRadio} name="diet" id="nodiet" value="nodiet"/>
+      <label htmlFor="homnivorus">Homnivore</label>
+      <input type="radio" onChange={handleRadio} name="diet" id="homnivorus" value="homnivorus"/>
+      <label htmlFor="vegetarian">Végétarien</label>
+      <input type="radio" onChange={handleRadio} name="diet" id="vegetarian" value="vegetarian"/>
+      <label htmlFor="vegan">Vegan</label>
+      <input type="radio" onChange={handleRadio} name="diet" id="vegan" value="vegan"/>
+
+      <button onClick={() => props.modifyIndex(3)}>VALIDER</button>
+    </form>
+  )
+}
